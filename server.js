08146071176209c9999c640fc2f72badef87350b
JonaCodes <jonathanfarache@gmail.com>
2016-07-28 14:43:03 +0300

    made nodeable

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+app = express();
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + "/index.html");
+})
+
+app.use("/node_modules", express.static(__dirname + '/node_modules'));
+app.use("/ang", express.static(__dirname + '/ang'));
+
+
+app.listen(9000);
