6d6f267626b4ef9c393bf8e10bb1d557e6659bcf
Brandon Bledsoe <brbledsoe.m@gmail.com>
2016-12-25 20:38:15 +0200

    simplifying app

--- allMoviesCtrl.js
+++
@@ -1,8 +0,0 @@
-app.controller('allMoviesCtrl', function($scope, moviesService) {
-
-	$scope.movies = moviesService.allMovies;
-	
-	$scope.addToMyCollection = function(movie){
-		moviesService.addToMyCollection(movie);
-	}	
-});
